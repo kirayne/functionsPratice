8bf6a27db75e19687369dab1c63fee56cab4db4f
kirayne <lbonifaciofreitas@gmail.com>
2021-06-02 17:29:12 -0300

    Praticing functions in JS

---
+++ functions.js
@@ -0,0 +1,12 @@
+ function add7(x){
+     return x + 7;
+}
+function multiply (x,y){
+    return x * y;
+}
+function capitalize(string){
+    return  string.charAt(0).toUpperCase() + string.slice(1);
+}
+function lastLetter(string){
+    return string.slice(-1);
+}
